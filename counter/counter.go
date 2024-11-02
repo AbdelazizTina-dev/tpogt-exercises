a17f595c77ad3c712c63dd5db8ce40b7b23726f6
Abdelaziz Tina <tinaabdelaziz8@gmail.com>
2024-11-02 19:50:35 +0100

    trying out the functional options approach

--- counter/counter.go
+++ counter/counter.go
@@ -2,25 +2,62 @@ package counter
 
 import (
 	"bufio"
+	"errors"
+	"fmt"
 	"io"
 	"os"
 )
 
 type Counter struct {
-	Input io.Reader
+	Input  io.Reader
+	Output io.Writer
 }
 
-func NewCounter() *Counter {
-	return &Counter{
-		Input: os.Stdin,
+type Option func(*Counter) error
+
+func WithInput(input io.Reader) Option {
+	return func(c *Counter) error {
+		if input == nil {
+			return errors.New("nil input reader")
+		}
+		c.Input = input
+		return nil
 	}
 }
 
-func Main() {
-	print(NewCounter().Count())
+func WithOutput(output io.Writer) Option {
+	return func(c *Counter) error {
+		if output == nil {
+			return errors.New("nil output reader")
+		}
+		c.Output = output
+		return nil
+	}
 }
 
-func (c *Counter) Count() int {
+func NewCounter(opts ...Option) (*Counter, error) {
+	c := &Counter{
+		Input:  os.Stdin,
+		Output: os.Stdout,
+	}
+	for _, opt := range opts {
+		err := opt(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return c, nil
+}
+
+func Main() {
+	c, err := NewCounter()
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println(c.Lines())
+}
+
+func (c *Counter) Lines() int {
 	lines := 0
 	input := bufio.NewScanner(c.Input)
 	for input.Scan() {
--- counter/counter_test.go
+++ counter/counter_test.go
@@ -10,10 +10,13 @@ import (
 func TestCounterCountLinesFromInput(t *testing.T) {
 	t.Parallel()
 	buf := bytes.NewBufferString("Hi\nMy Name is\nRudy\nI am from the Greyrat Family\nNice to meet you!")
-	c := counter.NewCounter()
+	c, err := counter.NewCounter(counter.WithInput(buf))
+	if err != nil {
+		t.Fatal(err)
+	}
 	c.Input = buf
 	want := 5
-	got := c.Count()
+	got := c.Lines()
 	if want != got {
 		t.Errorf("want %q, got %q", want, got)
 	}
